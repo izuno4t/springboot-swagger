266207ea47ec8fac01dcdea18a1b812aad24b06b
izuno4t <9846+izuno4t@users.noreply.github.com>
2022-06-29 16:32:08 +0900

    up

--- src/main/java/com/example/swagger/SwaggerConfig.java
+++ src/main/java/com/example/swagger/SwaggerConfig.java
@@ -1,33 +1,26 @@
 package com.example.swagger;
 
-import com.google.common.base.Predicate;
-import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 import java.util.ArrayList;
 
 @Configuration
-@EnableSwagger2
+@EnableSwagger2WebMvc
 public class SwaggerConfig {
     @Bean
     public Docket swaggerSpringMvcPlugin() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .paths(paths())
+                .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
-    }
-
-    private Predicate<String> paths() {
-        // ドキュメント生成の対象とするAPIのURLを指定
-        // この場合、「/user」で始まるAPIがドキュメント生成対象となる
-        return Predicates.or(Predicates.containsPattern("/user"));
     }
 
     private ApiInfo apiInfo() {
